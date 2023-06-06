85827892aa47cf4c6e5efad517478ee7fde07646
Denis <griniuk_denis@mail.ru>
2023-06-07 01:04:08 +0300

    Поменял значения местами уростил

--- lesson2/lesson_2_task_9.py
+++ lesson2/lesson_2_task_9.py
@@ -1,9 +1,16 @@
 var_1 = 37
 var_2 = 99
 
-temp = var_1
-var_1 = var_2
-var_2 = temp
+# temp = var_1
+# var_1 = var_2
+# var_2 = temp
+
+# print("var_1 =", var_1)
+# print("var_2 =", var_2)
+
+# или так
+
+var_1, var_2 = var_2, var_1
 
 print("var_1 =", var_1)
 print("var_2 =", var_2)
