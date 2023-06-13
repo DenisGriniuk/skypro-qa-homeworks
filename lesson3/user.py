e6748cffe802edb3f879de276f22e8491654aac9
Denis <griniuk_denis@mail.ru>
2023-06-13 11:35:46 +0300

    Вывел имя, фамилию, имя и фамилию вместе

---
+++ lesson3/user.py
@@ -0,0 +1,20 @@
+class User:
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def print_first_name(self):
+        print(self.first_name)
+
+    def print_last_name(self):
+        print(self.last_name)
+
+    def print_full_name(self):
+        print(self.first_name + ' ' + self.last_name)
+
+user = User("Denis", "Griniuk")
+user.print_first_name()  
+user.print_last_name()   
+user.print_full_name()   
+
