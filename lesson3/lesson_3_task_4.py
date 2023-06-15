9ca550d7e806ddc3c9eef1aafa3dd9d563e936ed
Denis <griniuk_denis@mail.ru>
2023-06-15 16:13:45 +0300

    нарисова медведя при помощи turtle

--- lesson3/lesson_3_task_4.py
+++ lesson3/lesson_3_task_4.py
@@ -1,20 +1,76 @@
-from turtle import *
+import turtle
 
-my_turtle = Turtle()
-my_turtle.speed(0)
-my_turtle.screen.setup(1200, 800)
+t = turtle.Turtle()
+t.speed(0)
+t.screen.setup(1200, 800)
 
-# Нарисовать квадрат
-def draw_rect(t):
-    for x in range(0, 4):
-        t.right(90)
-        t.forward(100)
+def ring(col, rad):
+	t.fillcolor(col)
+	t.begin_fill()
+	t.circle(rad)
+	t.end_fill()
 
-# Рисует квадраты в цикле
-for x in range(0, 360):
-    draw_rect(my_turtle)
-    my_turtle.right(1)
+# рисуем первое ухо
+t.up()
+t.setpos(-40, 80)
+t.down
+ring('brown', 20)
 
-# Необходимо, чтобы окно не закрывалось само, а только по клику
-my_turtle.screen.exitonclick()
-my_turtle.screen.mainloop()
+# рисуем второе ухо
+t.up()
+t.setpos(40, 80)
+t.down()
+ring('brown', 20)
+
+#рисуем лицо
+t.up()
+t.setpos(0, 35)
+t.down()
+ring('brown', 40)
+
+# рисуем первый глаз
+t.up()
+t.setpos(-22, 70)
+t.down
+ring('black', 8)
+
+# рисуем второй глаз
+t.up()
+t.setpos(22, 70)
+t.down()
+ring('black', 8)
+
+# рисуем зрачок
+t.up()
+t.setpos(-22, 73)
+t.down()
+ring('red', 4)
+
+# рисуем второй зрачок
+t.up()
+t.setpos(22, 73)
+t.down()
+ring('red', 4)
+
+# рисуем нос
+t.up()
+t.setpos(0, 55)
+t.down
+ring('black', 5)
+
+# рисуем рот
+t.up()
+t.setpos(0, 55)
+t.down()
+t.right(90)
+t.circle(5, 180)
+t.up()
+t.setpos(0, 55)
+t.down()
+t.left(360)
+t.circle(5, -180)
+t.hideturtle()
+
+t.screen.exitonclick()
+t.screen.mainloop()
+t.ht()
