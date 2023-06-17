f39f1a2a6561d3bd9a378d13e35c9038f4f246fe
Denis <griniuk_denis@mail.ru>
2023-06-17 12:52:28 +0300

    Убрал лишние переменные

--- lesson3/lesson_3_task_2.py
+++ lesson3/lesson_3_task_2.py
@@ -2,17 +2,11 @@ from smartphone import Smartphone
 
 catalog = []
 
-smartphone1 = Smartphone("Apple", "iPhone 13", "+79216665554")
-smartphone2 = Smartphone("Samsung", "Galaxy S22", "+79237623488")
-smartphone3 = Smartphone("OUKITEL", "WP12 Pro", "+79987654321")
-smartphone4 = Smartphone("Huawei", "Y5p", "+79354657684")
-smartphone5 = Smartphone("Xiaomi", "Mi 11", "+79555662387")
-
-catalog.append(smartphone1)
-catalog.append(smartphone2)
-catalog.append(smartphone3)
-catalog.append(smartphone4)
-catalog.append(smartphone5)
+catalog.append(Smartphone("Apple", "iPhone 13", "+79216665554"))
+catalog.append(Smartphone("Samsung", "Galaxy S22", "+79237623488"))
+catalog.append(Smartphone("OUKITEL", "WP12 Pro", "+79987654321"))
+catalog.append(Smartphone("Huawei", "Y5p", "+79354657684"))
+catalog.append(Smartphone("Xiaomi", "Mi 11", "+79555662387"))
 
 for smartphone in catalog:
     print(f"{smartphone.phone_brand} - {smartphone.phone_model}. {smartphone.subscriber_number}")
