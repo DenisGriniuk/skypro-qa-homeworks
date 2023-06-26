a8a019d50562bf70ee25455a368b8c1e72d71f31
Denis <griniuk_denis@mail.ru>
2023-06-26 11:16:35 +0300

    Протестировал калькулятор

---
+++ Sky/calculator_test.py
@@ -0,0 +1,42 @@
+from lesson4_calculator import Calculator
+
+calculator = Calculator()
+
+# ++
+# - -
+# - +
+# . .
+# n 0
+
+print("start")
+res = calculator.sum(4, 5)
+assert res == 9
+
+res = calculator.sum(-6, -10)
+assert res == -16
+
+res = calculator.sum(-6, 6)
+assert res == 0
+
+# doubt result?
+res = calculator.sum(5.6, 4.3)
+res = round(res, 1)
+print(res)
+assert res == 9.9
+
+res = calculator.sum(10, 0)
+assert res == 10
+
+res = calculator.div(10, 2)
+assert res == 5
+
+numbers = [1,2,3,4,5,6,7,8,9,5]
+res = calculator.avg(numbers)
+print(res)
+assert res == 5
+
+print("finish")
+
+# res = calculator.div(10, 0)
+# assert res == None
+
