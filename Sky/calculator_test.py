12e0c68346e28f39a77bc7149a43163c4a72458c
Denis <griniuk_denis@mail.ru>
2023-06-26 13:55:11 +0300

    Косметика

--- Sky/calculator_test.py
+++ Sky/calculator_test.py
@@ -3,8 +3,14 @@ from lesson4_calculator import Calculator
 
 calculator = Calculator()
 
-@pytest.mark.parametrize( 'num1, num2, result', [ (4, 5, 9), (-6, -10, -16), (-6, 6, 0), (5.61, 4.29, 9.9), (10, 0, 10)])
-def test_sum_positive_nums(num1, num2, result):
+@pytest.mark.parametrize( 'num1, num2, result', [ 
+    (4, 5, 9), 
+    (-6, -10, -16), 
+    (-6, 6, 0), 
+    (5.61, 4.29, 9.9), 
+    (10, 0, 10)
+    ])
+def test_sum_nums(num1, num2, result):
     calculator = Calculator()
     res = calculator.sum(num1, num2)
     assert res == result
@@ -42,7 +48,7 @@ def test_div_by_zero():
         calculator.div(10, 0)
     
 @pytest.mark.parametrize( 'nums, result', [ ([], 0), ([1,2,3,4,5,6,7,8,9,5], 5) ])
-def test_avg_empty_list(nums, result):
+def test_avg_list(nums, result):
     calculator = Calculator()    
     res = calculator.avg(nums)
     assert res == result
