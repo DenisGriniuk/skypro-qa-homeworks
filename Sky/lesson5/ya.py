1f6f61b5086694ae8fdb1a4851e57fc42e9105c9
Denis <griniuk_denis@mail.ru>
2023-07-08 12:21:35 +0300

    Открыл страницу яндекс

---
+++ Sky/lesson5/ya.py
@@ -0,0 +1,9 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://ya.ru")
+
+sleep(30)
