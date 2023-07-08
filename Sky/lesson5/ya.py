8be39d20a9a5a90755feb352f7ec8f4171f149b1
Denis <griniuk_denis@mail.ru>
2023-07-08 12:42:14 +0300

    Поработал с экраном

--- Sky/lesson5/ya.py
+++ Sky/lesson5/ya.py
@@ -4,6 +4,24 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.maximize_window()
+
 driver.get("https://ya.ru")
 
-sleep(30)
+sleep(5)
+
+driver.fullscreen_window()
+
+sleep(5)
+
+driver.save_screenshot('./ya.png')
+
+# driver.back()
+# driver.forward()
+# driver.refresh()
+
+# driver.set_window_size(640, 460)
+
+
+
+sleep(5)
