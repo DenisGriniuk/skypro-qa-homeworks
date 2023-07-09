3ebbf34b95de5e2d90485f6e07f1700394af8e55
Denis <griniuk_denis@mail.ru>
2023-07-09 12:51:57 +0300

    Нашёл книги по слову python

--- Sky/lesson5/labirint.py
+++ Sky/lesson5/labirint.py
@@ -1,17 +1,29 @@
 
-# найти книги по слову Python
+
 # собрать все карточки товаров
 # вывести в консоль инфо: название + автор + цена
+
 
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.maximize_window()
 
 # зайти на лабиринт
 driver.get("https://www.labirint.ru")
+
+# найти книги по слову Python
+search_locator = "#search-field"
+search_input = driver.find_element(By.CSS_SELECTOR, search_locator)
+# search_input.send_keys("Python")
+# search_input.send_keys(Keys.RETURN)
+search_input.send_keys("Python", Keys.RETURN)
+
 
 sleep(5)
