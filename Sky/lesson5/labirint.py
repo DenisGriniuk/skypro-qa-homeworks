a8f093012375b3b73e576798cc8fff4626c91e47
Denis <griniuk_denis@mail.ru>
2023-07-09 13:46:57 +0300

    косметика

--- Sky/lesson5/labirint.py
+++ Sky/lesson5/labirint.py
@@ -1,16 +1,10 @@
-
-
-
-
-
-
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.chrome.options import Options
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.maximize_window()
